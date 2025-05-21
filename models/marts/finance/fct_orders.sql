42b450352f022613f5b8f6a83a06cea8ce979729
SaiKumar Naidu <snaidu@intraedge.com>
2025-05-21 08:15:43 +0000

    updated in fct_orders model

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,41 +1,33 @@
-{{
-    config(
-        materialized='incremental'
+{{ config(materialized="incremental", incremental_strategy="append") }}
+
+with
+    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
+
+    payments as (select * from {{ ref("stg_stripe__payments") }}),
+
+    order_payments as (
+        select order_id, sum(case when status = 'success' then amount end) as amount
+
+        from payments
+        group by 1
+    ),
+
+    final as (
+
+        select
+            orders.order_id,
+            orders.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) as amount
+
+        from orders
+        left join order_payments using (order_id)
     )
-}}
 
-with orders as  (
-    select * from {{ ref ('stg_jaffle_shop__orders' )}}
-),
-
-payments as (
-    select * from {{ ref ('stg_stripe__payments') }}
-),
-
-order_payments as (
-    select
-        order_id,
-        sum (case when status = 'success' then amount end) as amount
-
-    from payments
-    group by 1
-),
-
- final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce (order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
-)
-
-select * from final
+select *
+from final
 
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
-    where order_date > (select max(order_date) from {{ this }}) 
+    where order_date > (select max(order_date) from {{ this }})
 {% endif %}
